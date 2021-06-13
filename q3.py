f6a3a76d9928d6c77da9086713c49c2ae1cc3dd3
shuhaida2019291274 <2019291274@isiswa.uitm.edu.my>
2021-06-13 08:09:29 +0000

    server calculator

---
+++ q3.py
@@ -0,0 +1,68 @@
+import math
+import socket
+import sys
+import time
+import errno
+from multiprocessing import Process
+
+ok_message = 'HTTP/1.0 200 OK\n\n'
+nok_message = 'HTTP/1.0 404 NotFound\n\n'
+
+def process_start(s_sock):
+    s_sock.send(str.encode('\nAIDIOUS CALCULATOR'))
+    while True:
+        data = s_sock.recv(2048)
+        data = data.decode("utf-8")
+
+        try:
+            operation, val = data.split(":")
+            opt = str(operation)
+            n = float(val)
+
+            if opt[0] == 'l':
+                opt = 'Logarithmic'
+                ans = math.log10(n)
+            elif opt[0] == 's':
+                opt = 'Square Root'
+                ans = math.sqrt(n)
+            elif opt[0] == 'e':
+                opt = 'Exponential'
+                ans = math.exp(n)
+            else:
+                answer = ('error...')
+
+            sendAns = (str(opt)+ '['+ str(n) + ']= ' + str(ans))
+            print ('\nCalculation successfully Done!')
+        except:
+            print ('Connection Terminated')
+            sendAns = ('Connection Terminated')
+
+        if not data:
+            break
+
+        s_sock.send(str.encode(str(sendAns)))
+    s_sock.close()
+
+if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("",8888))
+    print("listening...")
+    s.listen(3)
+    try:
+        while True:
+            try:
+                s_sock, s_addr = s.accept()
+                p = Process(target=process_start, args=(s_sock,))
+                print("Connected to the client...")
+                p.start()
+
+            except socket.error:
+
+                print('Socket error')
+
+    except Exception as e:
+                print("an exception occurred!")
+                print(e)
+                sys.exit(1)
+    finally:
+     	   s.close()
